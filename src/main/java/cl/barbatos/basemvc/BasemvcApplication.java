8eae02dc6899449628e3c002025dcf25a4194ff5
Esteban Lillo <estebanlilloc@gmail.com>
2023-09-30 04:38:58 -0300

    basemvc initial config

---
+++ src/main/java/cl/barbatos/basemvc/BasemvcApplication.java
@@ -0,0 +1,13 @@
+package cl.barbatos.basemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasemvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasemvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/barbatos/basemvc/BasemvcApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.barbatos.basemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasemvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
