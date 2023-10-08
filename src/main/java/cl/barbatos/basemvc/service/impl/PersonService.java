25dd3652b4fcc9d8366d56e2e553f43766b69c30
Esteban Lillo <estebanlilloc@gmail.com>
2023-10-08 17:58:59 -0300

    Update person service

--- src/main/java/cl/barbatos/basemvc/service/impl/PersonService.java
+++ src/main/java/cl/barbatos/basemvc/service/impl/PersonService.java
@@ -1,7 +1,6 @@
 package cl.barbatos.basemvc.service.impl;
 
 import cl.barbatos.basemvc.model.dto.PersonDTO;
-import cl.barbatos.basemvc.model.dto.RoleDTO;
 import cl.barbatos.basemvc.model.entity.Person;
 import cl.barbatos.basemvc.model.entity.Role;
 import cl.barbatos.basemvc.repository.AddressRepository;
@@ -9,6 +8,8 @@ import cl.barbatos.basemvc.repository.PersonRepository;
 import cl.barbatos.basemvc.repository.RoleRepository;
 import cl.barbatos.basemvc.service.IPersonService;
 import cl.barbatos.basemvc.util.DtoConverter;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -35,6 +36,7 @@ public class PersonService implements IPersonService {
     }
 
     @Override
+    @Transactional
     public boolean createUser(PersonDTO person) {
         try {
 
@@ -50,6 +52,50 @@ public class PersonService implements IPersonService {
             return true;
         } catch (DataAccessException ex) {
             throw new RuntimeException("Error al guardar la persona", ex);
+        }
+    }
+
+
+
+    @Transactional // Asegura una transacción para actualizar la persona y sus roles
+    public boolean updateUser(Long userId, PersonDTO updatedPerson) {
+        try {
+            // Recupera el usuario existente de la base de datos
+            Person existingPerson = personRepository.findById(userId)
+                    .orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
+
+            // Actualiza los campos del usuario con la información proporcionada en PersonDTO
+            existingPerson.setName(updatedPerson.getName());
+            existingPerson.setEmail(updatedPerson.getEmail());
+            // ...
+
+            // Recupera la lista de roles actuales del usuario
+            List<Role> existingRoles = existingPerson.getRoles();
+
+            // Recupera la lista de roles nuevos del DTO y conviértelos en entidades Role
+            List<Role> updatedRoles = updatedPerson.getRoles().stream()
+                    .map(roleRepository::findByRoleName)
+                    .toList();
+
+            // Determina las diferencias entre las listas de roles
+            List<Role> rolesToAdd = updatedRoles.stream()
+                    .filter(role -> !existingRoles.contains(role))
+                    .toList();
+
+            List<Role> rolesToRemove = existingRoles.stream()
+                    .filter(role -> !updatedRoles.contains(role))
+                    .toList();
+
+            // Agrega los nuevos roles al usuario y elimina los roles que ya no están presentes
+            existingPerson.getRoles().addAll(rolesToAdd);
+            existingPerson.getRoles().removeAll(rolesToRemove);
+
+            // Guarda el usuario actualizado en la base de datos
+            personRepository.save(existingPerson);
+
+            return true;
+        } catch (DataAccessException ex) {
+            throw new RuntimeException("Error al actualizar la persona", ex);
         }
     }
 
